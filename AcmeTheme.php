3ec476ac87184f14b7d0cb240581b1ab1e46d5ae
David Sanchez <david.sanchez@nventive.com>
2019-02-08 13:15:32 -0500

    Add main class extends `Theme`

---
+++ AcmeTheme.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Harmony\Theme\AcmeTheme;
+
+use Harmony\Sdk\Theme\Theme;
+
+/**
+ * Class AcmeTheme
+ *
+ * @package Harmony\Theme\AcmeTheme
+ */
+class AcmeTheme extends Theme
+{
+
+    /** Constants */
+    const NAME        = 'Harmony Acme';
+    const DESCRIPTION = 'HarmonyCMS Acme starter theme';
+}
